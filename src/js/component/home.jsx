7475b2a2b8a8edba8c0827e68c3f4e6f6638c154
yonilhp <55214610+yonilhp@users.noreply.github.com>
2024-08-12 17:59:50 +0000

    termine el trabajo del clock

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,63 @@
-import React from "react";
+// src/component/home.jsx
+import React, { useState, useEffect } from "react";
+import SecondsCounter from './secondsCounter'; // Importa el nuevo componente
+import { FaClock } from 'react-icons/fa';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds(prevSeconds => prevSeconds + 1);
+    }, 1000);
+
+    return () => clearInterval(interval); // Limpia el intervalo al desmontar
+  }, []);
+
+  return (
+
+    <div className="container text-center">
+      <div className="row g-2 g-lg-3 bg-black justify-content-center">
+        <div className="col-12 col-sm-1 d-flex justify-content-center align-items-center ">
+          <div className="p-3 mt-3 mb-3 bg-dark rounded-3 text-white">
+			<h1>
+				<FaClock/>
+			</h1>
+		  </div>
+        </div>
+        <div className="col-12 col-sm-1 d-flex justify-content-center align-items-center">
+          <div className="p-3 mt-3 mb-3 bg-dark rounded-3 text-white">
+			<h1>0</h1>
+		  </div>
+        </div>
+        <div className="col-12 col-sm-1 d-flex justify-content-center align-items-center">
+          <div className="p-3 mt-3 mb-3 bg-dark rounded-3 text-white">
+		  <h1>0</h1>
+		  </div>
+        </div>
+        <div className="col-12 col-sm-1 d-flex justify-content-center align-items-center">
+          <div className="p-3 mt-3 mb-3 bg-dark rounded-3 text-white">
+		  <h1>0</h1>
+		  </div>
+        </div>
+        <div className="col-12 col-sm-1 d-flex justify-content-center align-items-center">
+          <div className="p-3 mt-3 mb-3 bg-dark rounded-3 text-white">
+		  <h1>0</h1>
+		  </div>
+        </div>
+        <div className="col-12 col-sm-1 d-flex justify-content-center align-items-center">
+          <div className="p-3 mt-3 mb-3 bg-dark rounded-3 text-white">
+		  <h1>0</h1>
+		  </div>
+        </div>
+        <div className="col-12 col-sm-1 d-flex justify-content-center align-items-center">
+          <div className="p-3 mt-3 mb-3 bg-dark rounded-3 text-white">
+		  <h1><SecondsCounter seconds={seconds}/></h1>
+		  </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/js/component/secondsCounter.jsx
@@ -0,0 +1,14 @@
+// src/component/secondsCounter.jsx
+import React from 'react';
+
+
+const SecondsCounter = ({ seconds }) => {
+  return (
+    <div className="text-center">
+      {seconds}
+    </div>
+  );
+};
+
+export default SecondsCounter;
+
